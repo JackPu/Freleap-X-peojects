5177e3b1af420b84a1caeeb80869ba64a9a7a03f
JingYi_QiuShui <kakashjack@gmail.com>
2016-06-18 14:24:02 +0800

    add url test

--- js/reg.js
+++ js/reg.js
@@ -10,3 +10,10 @@ if(/[\u4E00-\u9FA5]+/g.test(str)){
 
 // test iOS device
 /(iPad|iPhone|iPod)/g.test( navigator.userAgent );
+
+
+var url= "https://www.google.com";
+if(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(url)) {
+  return true;
+  
+}
