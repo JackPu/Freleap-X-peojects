6e6aa8f7f3b7e6baafaa4eb6e2e9e578423ea663
蒲小花 <kakashjack@gmail.com>
2016-11-14 20:03:16 +0800

    Update reg.js

--- js/reg.js
+++ js/reg.js
@@ -17,3 +17,18 @@ if(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:
   return true;
   
 }
+
+
+window.getSearch = function(variable) {
+  var parsedSearch;
+  parsedSearch = window.parsedSearch || (function() {
+    var match, re, ret;
+    re = /\??(.*?)=([^\&]*)&?/gi;
+    ret = {};
+    while (match = re.exec(document.location.search)) {
+      ret[match[1]] = match[2];
+    }
+    return window.parsedSearch = ret;
+  })();
+  return parsedSearch[variable];
+};
