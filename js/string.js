7f9221499cbae2c83d50f02e2b56f4c1d6751e19
蒲小花 <kakashjack@gmail.com>
2017-01-04 14:20:15 +0800

    Update string.js
    add dash - camelcase - underscore

--- js/string.js
+++ js/string.js
@@ -1,1 +1,12 @@
 
+String.prototype.toCamel = function(){
+	return this.replace(/(\-[a-z])/g, function($1){return $1.toUpperCase().replace('-','');});
+};
+
+String.prototype.toDash = function(){
+	return this.replace(/([A-Z])/g, function($1){return "-"+$1.toLowerCase();});
+};
+
+String.prototype.toUnderscore = function(){
+	return this.replace(/([A-Z])/g, function($1){return "_"+$1.toLowerCase();});
+};
