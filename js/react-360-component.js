aacf56c1547a9eb6ee784d276e09135f9e33c91c
蒲小花 <kakashjack@gmail.com>
2019-08-04 22:47:17 +0800

    Create react-360-component.js

---
+++ js/react-360-component.js
@@ -0,0 +1,13 @@
+import {
+  StyleSheet,
+  View,
+} from 'react-360';
+import BaseComponent from '../component';
+
+class Dashboard extends BaseComponent {
+  render() {
+    return (
+      <View style={styles.dashboard}></View>
+    )
+  }
+}
